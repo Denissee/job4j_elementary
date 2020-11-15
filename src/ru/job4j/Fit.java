40ca40f50e62fb8d90fcde8a17adbd442ee78014
Denissee <denis1996>
2020-11-15 18:15:23 +0500

    6. Идеальный вес [#400165]

---
+++ src/ru/job4j/Fit.java
@@ -0,0 +1,20 @@
+package ru.job4j;
+
+public class Fit {
+    public static double manWeight (short mheight){
+        double rsl = (mheight - 100) * 1.15;
+        return rsl;
+    }
+    public static double womenWeight (short height){
+        double rsl = (height - 100)*1.15;
+        return  rsl;
+    }
+    public static void main (String [] args){
+       short mheight = 172;
+       double man = manWeight(mheight);
+        System.out.println("Идеальный вес для мужчины составит "+man+"кг.");
+        short height= 165;
+       double women = Fit.womenWeight(height);
+       System.out.println("Идеальный вес для женщины составит "+women+"кг.");
+    }
+}
