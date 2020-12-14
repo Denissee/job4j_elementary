46cc2812d35410d8c66360f40164f6e0a51ff745
Denissee <denis1996>
2020-12-14 23:27:45 +0500

    4.5. Отладка программы в IDEA [#400111]

--- test/ru/job4j/condition/SqAreaTest.java
+++ test/ru/job4j/condition/SqAreaTest.java
@@ -8,20 +8,20 @@ import static org.junit.Assert.*;
 public class SqAreaTest {
 
     @Test
-    public void sqarea() {
-        int expected = 2;
-        int k = 6;
-        int p = 2;
+    public void whenp4k1s1() {
+        int expected = 1;
+        int k = 1;
+        int p = 4;
         double out = SqArea.sqarea(k, p);
         Assert.assertEquals(expected, out, 0.01);
 
     }
 
     @Test
-    public void second() {
-        int expected = 0;
-        int k = 8;
-        int p = 4;
+    public void whenp6k2s2() {
+        int expected = 2;
+        int k = 2;
+        int p = 6;
         double out = SqArea.sqarea(k, p);
         Assert.assertEquals(expected, out, 0.01);
     }
