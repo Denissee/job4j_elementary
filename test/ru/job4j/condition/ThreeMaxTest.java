d24b382ef1a7482dc44785ca4b88b49484b0b822
Denissee <denis1996>
2020-12-19 15:41:37 +0500

    4.5. Отладка программы в IDEA [#400111]

--- test/ru/job4j/condition/ThreeMaxTest.java
+++ test/ru/job4j/condition/ThreeMaxTest.java
@@ -37,7 +37,7 @@ public class ThreeMaxTest {
 
     @Test
     public void firstEqThird() {
-        int rsl = ThreeMax.max(100, 1, 100);
+        int rsl = ThreeMax.max(100, 100, 100);
         assertThat(rsl, is(100));
     }
 
