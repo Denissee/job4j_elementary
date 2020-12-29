e630d050870971ff1c1949471300986d38f823e9
Denissee <denis1996>
2020-12-29 22:44:55 +0500

    5.5. Простое число [#400225]

---
+++ test/ru/job4j/Loop/ChekPrimeNumberTest.java
@@ -0,0 +1,25 @@
+package ru.job4j.Loop;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class ChekPrimeNumberTest {
+
+    @Test
+    public void when5() {
+        boolean rsl = ChekPrimeNumber.check(5);
+        assertThat(rsl, is(true));
+    }
+    @Test
+    public void when4() {
+        boolean rsl = ChekPrimeNumber.check(4);
+        assertThat(rsl, is(false));
+    }
+    @Test
+    public void when1() {
+        boolean rsl = ChekPrimeNumber.check(1);
+        assertThat(rsl, is(false));
+    }
+}
