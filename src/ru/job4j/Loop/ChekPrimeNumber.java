6362d78e2e98788605d3bd0a0c396a66653e000c
Denissee <denis1996>
2020-12-30 22:20:26 +0500

    5.4. Протеиновая диета [#400222]

--- src/ru/job4j/Loop/ChekPrimeNumber.java
+++ src/ru/job4j/Loop/ChekPrimeNumber.java
@@ -3,11 +3,8 @@ package ru.job4j.Loop;
 public class ChekPrimeNumber {
     public static boolean check(int number) {
         boolean prime = false;
-        int temp;
-        int num = 0;
         for (int i = 1; i <= number / 2; i++) {
-            num = number % i;
-            if (num == 0) {
+            if (number % i == 0) {
                 prime = true;
                 break;
             }
