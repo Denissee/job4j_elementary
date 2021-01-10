604c460c6b1849fe4c98bf56207a5fc7cb617be5
Denissee <denis1996>
2021-01-10 19:51:12 +0500

    5.7. Ипотека [#400224]

--- src/ru/job4j/Loop/Mortage.java
+++ src/ru/job4j/Loop/Mortage.java
@@ -3,8 +3,6 @@ package ru.job4j.Loop;
 public class Mortage {
     public static int year(double amount, double salary, double percent) {
         int year = 0;
-        double x, y, z;
-        x = percent / 100;
         while (amount > 0) {
             amount = (percent / 100 * amount + amount) - salary;
            year++;
