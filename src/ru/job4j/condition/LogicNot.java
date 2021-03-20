0a428cd2de6b9900a85b46926327c9adedcd2cb4
Denissee <denis1996>
2021-03-20 19:01:07 +0500

    4.3.3. Логическое отрицание ! [#400123]

--- src/ru/job4j/condition/LogicNot.java
+++ src/ru/job4j/condition/LogicNot.java
@@ -14,19 +14,19 @@ public class LogicNot {
     }
 
     public static boolean notEven(int num) {
-        return !notEven(num);
+        return !(num % 2 == 0);
     }
 
     public static boolean notPositive(int num) {
-        return !notPositive(num);
+        return !(num > 0);
     }
 
     public static boolean notEvenAndPositive(int num) {
-        return !notEven(num) && isPositive(num);
+        return !(num % 2 == 0) && !(num < 0);
     }
 
     public static boolean evenOrNotPositive(int num) {
-        return !notEven(num) || !notPositive(num);
+        return !(num % 3 == 0) || !(num > 0);
     }
 
     public static void main(String[] args) {
@@ -34,14 +34,14 @@ public class LogicNot {
         System.out.println("Остаток равен 0");
         LogicNot.isPositive(6);
         System.out.println("Положительное число равно 6");
-        LogicNot.notEven(8);
-        System.out.println("Отрицательное число равно -8");
-        LogicNot.notPositive(7);
+        LogicNot.notEven(7);
+        System.out.println("число 7 не положительное число");
+        LogicNot.notPositive(-7);
         System.out.println("Отрицательное число равно -7");
         LogicNot.notEvenAndPositive(5);
-        System.out.println("Отрицательное число равно 5");
-        LogicNot.evenOrNotPositive(2);
-        System.out.println("Отрицательное число равно 2");
+        System.out.println("Число 5 нечетное и положительное");
+        LogicNot.evenOrNotPositive(-3);
+        System.out.println(" Число -3 лишь не положительное");
 
     }
 }
